0e9d26674e45017553b91ade1221b055fff5f360
muhfikri11 <88622753+muhfikri11@users.noreply.github.com>
2022-11-21 01:19:02 +0800

    Add files via upload

---
+++ ContentActivity.java
@@ -0,0 +1,54 @@
+package com.f55121105.sqlitefik;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.f55121105.sqlitefik.databinding.ActivityContentBinding;
+import com.f55121105.sqlitefik.databinding.ActivityContentBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ContentActivity extends AppCompatActivity {
+    private ActivityContentBinding binding;
+    private final BottomNavigationView.OnNavigationItemSelectedListener
+            mOnNavigationItemSelectedListener = new BottomNavigationView.
+            OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    switchFragment(fragment);
+                    return true;
+                case R.id.navigation_dashboard:
+                    fragment = new DashboardFragment();
+                    switchFragment(fragment);
+                    return true;
+                case R.id.navigation_account:
+                    fragment = new AccountFragment();
+                    switchFragment(fragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityContentBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        if (savedInstanceState == null) {
+            binding.navigation.setSelectedItemId(R.id.navigation_home);
+        }
+    }
+    private void switchFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container_layout, fragment, fragment.getClass()
+                        .getSimpleName()).commit();
+    }
+}
